d37caf347d1bed47134f6bb713064db43e6847bf
Tamim101 <tamimkhan7133@gmail.com>
2025-06-16 22:53:02 +0600

    update code all

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,5 +1,4 @@
 //lead code or haker rank 
-
 void insertion_sort(int A[],int n){
     int i,j,item;
     for (i=1;i<n;i++){
@@ -11,4 +10,34 @@ void insertion_sort(int A[],int n){
     }
     A[i+1] = item;    //item is empity
     }
+}
+
+#include<stdio.h>
+int main(){
+    int num[100];
+    int n,i,j;
+    int item;
+    printf("ENTER ELEMENT = ");
+    scanf("%d", &n);
+
+    printf("ENTER %d NUMBER = ", n);
+    for(i=0;i<n;i++){
+        scanf("%d",&num[i]);
+    }
+    for(i=0;i<n;i++){
+        item = num[i];
+        j = i-1;
+        while(j>=0 && num[j]>item){
+              num[j+1] = num[j];
+              j = j-1;
+        } 
+        num[j+1] = item;     
+    }
+    
+    printf("Sorted array: ");
+    for(i = 0; i < n; i++){
+        printf("%d ", num[i]);
+    }
+    printf("\n");
+    return 0;
 }
