451bf2f452a9a0f8b69cf822f7fb8bc458abb338
Tamim101 <tamimkhan7133@gmail.com>
2025-06-16 21:43:19 +0600

    update file

--- entype.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "entype.h"  // Include your own header
-
-void my_function() {
-    printf("Hello from entype.c\n");
-}
--- entype.h
+++
@@ -1,6 +0,0 @@
-#ifndef ENTYPE_H
-#define ENTYPE_H
-
-void my_function();  // Declare your function here
-
-#endif
