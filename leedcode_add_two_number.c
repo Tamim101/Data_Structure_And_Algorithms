9d4b3f430ef5a48c1d39fec30ad476ef66a34830
Tamim101 <tamimkhan7133@gmail.com>
2025-06-28 17:15:54 +0600

    update leadcode problem solved add TwoSum 3

---
+++ leedcode_add_two_number.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode* next;
+};
+
+struct ListNode* createNode(int value) {
+    struct ListNode* node = (struct ListNode*)malloc(sizeof(struct ListNode));
+    node->val = value;
+    node->next = NULL;
+    return node;
+}
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;
+    struct ListNode* tail = &dummy;
+    dummy.next = NULL;
+    int carry = 0;
+
+    while (l1 != NULL || l2 != NULL || carry > 0) {
+        int sum = carry;
+
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+        int digit = sum % 10;
+
+        tail->next = createNode(digit);
+        tail = tail->next;
+    }
+
+    return dummy.next;
+}
+
+void printList(struct ListNode* head) {
+    while (head != NULL) {
+        printf("%d", head->val);
+        if (head->next != NULL) {
+            printf(" -> ");
+        }
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int listToInt(struct ListNode* node) {
+    int num = 0;
+    int place = 1;
+    while (node != NULL) {
+        num += node->val * place;
+        place *= 10;
+        node = node->next;
+    }
+    return num;
+}
+
+int main() {
+    struct ListNode* l1 = createNode(2);
+    l1->next = createNode(4);
+    l1->next->next = createNode(3);
+
+    struct ListNode* l2 = createNode(5);
+    l2->next = createNode(6);
+    l2->next->next = createNode(4);
+
+    struct ListNode* result = addTwoNumbers(l1, l2);
+
+    int total = listToInt(l1) + listToInt(l2);
+    printf("Total: %d\n", total);  // Output: 807
+
+    printf("Result: ");
+    printList(result);  // Should print: 7 -> 0 -> 8
+
+    return 0;
+}
+
+
+
+        
+    
