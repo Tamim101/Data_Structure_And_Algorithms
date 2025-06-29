3fe9c1ace5bef1b16c6e58f312e58f4a9557cef9
Tamim101 <tamimkhan7133@gmail.com>
2025-06-29 18:42:24 +0600

    not fix

---
+++ quick_sort.c
@@ -0,0 +1,22 @@
+//quick Sort 1 5 7 38 9 5
+#include<stdio.h>
+int main(){
+    int num[100];
+    int n,i;
+    int low,high;
+    printf("ENTER THE ELEMENT = ");
+    scanf("%d",&n);
+    printf("ENTER THE %d SORTED NUMBER = ",n);
+    for(i=0;i<n;i++){
+        scanf("%d",&num[i]);
+    }
+    for(i=0;i<n;i++){
+        if(low >= high){
+        return;
+    }
+        int p;
+        
+    }
+    
+    
+}
