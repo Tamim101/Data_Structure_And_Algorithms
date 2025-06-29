19c40125e069e29a53ad080b5af71952584f401d
Tamim101 <tamimkhan7133@gmail.com>
2025-06-29 19:25:57 +0600

    update quick sort

--- quick_sort.c
+++ quick_sort.c
@@ -1,22 +1,56 @@
-//quick Sort 1 5 7 38 9 5
-#include<stdio.h>
-int main(){
-    int num[100];
-    int n,i;
-    int low,high;
-    printf("ENTER THE ELEMENT = ");
-    scanf("%d",&n);
-    printf("ENTER THE %d SORTED NUMBER = ",n);
-    for(i=0;i<n;i++){
-        scanf("%d",&num[i]);
+#include <stdio.h>
+
+// Function to swap two elements
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Partition function
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];  // last element as pivot
+    int i = low - 1;
+
+    for(int j = low; j < high; j++) {
+        if(arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
     }
-    for(i=0;i<n;i++){
-        if(low >= high){
-        return;
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+// Quick Sort function (recursive)
+void quick_sort(int arr[], int low, int high) {
+    if(low < high) {
+        int pi = partition(arr, low, high);
+
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
     }
-        int p;
-        
-    }
+}
+
+int main() {
+    int arr[100], n;
     
-    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d numbers: ", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    quick_sort(arr, 0, n - 1);
+
+    printf("Sorted array (Quick Sort): ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
