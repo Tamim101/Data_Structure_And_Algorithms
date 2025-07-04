d5fa0623bd438cb6b5c6b53ff25a567077994514
Tamim101 <tamimkhan7133@gmail.com>
2025-07-04 21:25:48 +0600

    update problem

--- leedcode_Longest_substring.c
+++ leedcode_Longest_substring.c
@@ -30,3 +30,39 @@ int main() {
     printf("Length of longest substring without repeating characters: %d\n", result);
     return 0;
 }
+
+//abcabd output is 4 ok 
+char main_leanth_of_list(char* s){
+    int max_leanth[256];
+    for(int i =0; i< 256;i++){
+        max_leanth[i] = -1;
+        
+    }
+    int start = 0;
+    int max = 0; 
+    for(int i=0;s[i] != '\0';i++){
+        char ch = s[i];
+        
+        if(max_leanth[(unsigned char)ch]>= start){
+            start = max_leanth[(unsigned char)ch] + 1;
+        }
+        max_leanth[(unsigned char)ch] = i;
+        int current = i - start + 1;
+        printf("%d",current);
+        if(current > max){
+            max = current;
+            
+        }  
+    }
+    return max;
+    
+
+}
+// int main(){
+//     char str[100];
+//     printf("enter string = ");
+//     scanf("%s",str);
+//     int result =  main_leanth_of_list(str);
+//     printf("len of the string = %d\n",result);
+//     return 0;
+// }
