25a8e60e7326b5a0eff4f721ef12452af13bdab2
Tamim101 <tamimkhan7133@gmail.com>
2025-07-24 21:25:25 +0600

    update

--- leetcode_remove_duplicates.c
+++ leetcode_remove_duplicates.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int remove_duplicates(int* nums, int numsSize){
     if(numsSize == 0){
-        return 0;  // if number is bank so number size or len number is 0
+        return 0;  // if number is blank so number size or len number is 0
     }
-    int i = 0 ; // first index is 0 that means input 112233
+    int i = 0 ; // first index is 0 len number is 0 that means input 112233
     for(int j= 1; j<numsSize; j++){
                                  //j =1 thats mean index 1 
         if(nums[j] != nums[i]){
                             // number 1 == 1 is same but 1 != 2  
           i++ ;   // so its take 2
           nums[i] = nums[j] ;
+          printf("num i = %d\n num j %d\n",nums[i],nums[j]);
         }
     }
     return i+1 ; // i incriment 
