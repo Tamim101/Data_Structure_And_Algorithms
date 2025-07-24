c8943f6181136f18d65a3b9112c753a5dd4c8428
Tamim101 <tamimkhan7133@gmail.com>
2025-07-25 00:13:59 +0600

    pointer update

--- pointer.c
+++ pointer.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 int main(){
-    int a ;
+    int a = 1025;
     int *p;
     p = &a;
-    printf("A = %p\n",p);
+    printf("size of intger is %d bytes\n",sizeof(int));
+    printf("Address = %d\n, value = %d\n",p,*p);
+    printf("Address = %d\n, value = %d\n",p+1,*p+1);
+
+    char *st0;
+    st0 = (char*)p;
+    printf("size of char is %d bytes\n",sizeof(char));
+    printf("Address = %d\n, value = %d\n",st0,*st0);
+    printf("Address = %d\n, value = %d\n",st0+1,*st0+1);
     return 0;
 }
