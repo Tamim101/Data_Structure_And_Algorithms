77636f8ef988dc047ac89af645a1755a4dbbee02
Tamim101 <tamimkhan7133@gmail.com>
2025-08-06 19:34:26 +0600

    update solved next problem

---
+++ Search_insert_Position.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int serchInsert(int* nums,int numsSize, int target){
+    int i;
+    for(i = 0; i < numsSize; i++){
+        if(nums[i] >= target){
+            printf("target  = %d",i);
+            return i;
+        }
+    }
+    return numsSize;
+        
+    
+}
+int main(){
+    int nums[] = {1, 3, 5, 6};
+    int target = 7;
+    int len  = sizeof(nums) / sizeof(nums[0]);
+    int new_len_number  = serchInsert(nums,len,target);
+    printf("nuw len number = %d\n",new_len_number);
+    printf("array = ");
+    for(int i = 0 ; i< new_len_number;i++){
+        printf("%d\n",nums[i]);
+    }
+    printf("\n");
+    return 0;
+
+}
