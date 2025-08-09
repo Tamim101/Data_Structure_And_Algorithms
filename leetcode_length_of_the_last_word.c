a2de0f0b6ae9908cfe8bbf0f8c4cfaf3ecf717ec
Tamim101 <tamimkhan7133@gmail.com>
2025-08-09 21:47:05 +0600

    update solved next problemss

--- leetcode_length_of_the_last_word.c
+++ leetcode_length_of_the_last_word.c
@@ -2,12 +2,27 @@
 #include<string.h>
 int length_of_last_world(char* s){
     int i,j;
+    int last_length = 0;
+    int current_length = 0;
     for (i = 0; s[i] != '\0';i++){
+        if(s[i] != ' '){
+          current_length++;
+          last_length = current_length;
+        }else{
+            current_length = 0;
+        }
     }
-    return i;
+    return last_length;
 }
 int main() {
-    char *str = "hello";
+    char *str = "hello world now i have ";
+    int len = length_of_last_world(str);
+    printf("Length of '%s' is %d\n", str, len);
+    return 0;
+}
+
+int main() {
+    char *str = "hello world now i have ";
     int len = length_of_last_world(str);
     printf("Length of '%s' is %d\n", str, len);
     return 0;
