a84dbe0f12e6d10845f447466e1a7a2677815b28
Tamim101 <tamimkhan7133@gmail.com>
2025-08-12 17:01:37 +0600

    updates

--- plus_one.c
+++ plus_one.c
@@ -10,13 +10,3 @@ int* plusOne(int* digits, int digitsSize, int* returnSize) {
     }
 }
 
-
-int* plusOne(int* digits, int digitsSize, int* returnSize) {
-    int *digits = (int *)malloc(sizeof(digits));
-    int i;
-    for(i = 0; i < digitsSize; i++){
-        if(digits[i] != ','){
-            digits[i] = digits[i]+1;
-        }
-    }
-}
