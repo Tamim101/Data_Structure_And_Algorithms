4ddfa3595e09cb55ccb55f48a9caf457fa063e45
Tamim101 <tamimkhan7133@gmail.com>
2025-08-14 19:35:39 +0600

    update data

--- plus_one.c
+++ plus_one.c
@@ -26,6 +26,30 @@ int main(){
 }
 
 #include<stdio.h>
-int insert_data(int nums[],int length, int key,int capacity){
+#include <stdint.h> // uint8_t 
+int insert_data(uint8_t nums[],uint8_t length, uint8_t key,uint8_t capacity){
+    if(length >= capacity){
+        printf("length = %d\n",length);
+        return length;
+    }
+    nums[length] = key;
+    printf("last = %d\n ",length+1);
+    return length+1;
+
+}
+int main(){
+    uint8_t nums[] = {1,3,4,5,6,7,3};
+    uint8_t capacity = sizeof(nums) / sizeof(nums[0]);
+    uint8_t length = 6;
+    uint8_t key = 54;
+    for(uint8_t i = 0; i<capacity;i++){
+        printf("insert array = %d\n",nums[i]);
+    }
+    uint8_t call_array = insert_data(nums,length,key,capacity);
+    for(uint8_t i = 0; i< call_array; i++){
+        printf("insert at the end %d\n ",nums[i]);
+    }
+    printf("\n");
+    return 0;
 
 }
