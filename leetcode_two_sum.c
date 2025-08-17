b3e6d5fa799e31f1117d4c80ee3734ae8c025c49
Tamim101 <tamimkhan7133@gmail.com>
2025-08-17 11:13:14 +0600

    update two sum leetcode problems

--- leetcode_two_sum.c
+++ leetcode_two_sum.c
@@ -20,6 +20,26 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     returnSize =0;
     return NULL;
 }
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int i;
+    for(i = 0; i< numsSize;i++){
+        for(int j = i+1; j< numsSize;j++){
+            if(nums[i] + nums[j] == target){
+                int *main_value = (int*)malloc(2* sizeof(int));
+                if(main_value == NULL){
+                    exit(1);
+                }
+                main_value[0] = i;
+                main_value[1] = j;
+                *returnSize = 2;
+                return main_value;
+            }
+
+        }
+    }
+    returnSize =0;
+    return NULL;
+}
 int main(){
     int nums[] = {2,7,11,15};
     int len = sizeof(nums) / sizeof(nums[0]);
