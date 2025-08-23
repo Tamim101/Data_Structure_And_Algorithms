e463ef23573bc8c3540994f1bac7dfc7c509b69e
Tamim101 <tamimkhan7133@gmail.com>
2025-08-23 17:31:34 +0600

    update median

---
+++ leetcode_median_two_sorted_array.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    for(int i = 0; i < nums1Size; i++){
+        for(int j = i+1; i < nums2Size; j++){
+            
+        }
+    }
+}
