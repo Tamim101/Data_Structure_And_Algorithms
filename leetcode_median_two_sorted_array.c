e820ba726b302a385a4c0144f76e40f6468a4054
Tamim101 <tamimkhan7133@gmail.com>
2025-08-24 11:44:36 +0600

    never solved

--- leetcode_median_two_sorted_array.c
+++ leetcode_median_two_sorted_array.c
@@ -12,7 +12,7 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
         printf("%d\n",result[i]);
 
     }
-    int main_result = (nums1Size + nums2Size);
+    int main_result = (nums1Size + nums2Size);   // 1234 marged array
     float mid;
     if(main_result % 2 == 0){
         mid =  (result[main_result / 2 -1] + result[main_result /2]) / 2.0000;
@@ -37,4 +37,37 @@ int main(){
 
     return 0;
 
+}
+# include<stdio.h>
+double array_marged_impliment(int* array1, int arraySize1, int* array2, int arraySize2){
+    int result[arraySize1 + arraySize2]; // length number added 
+    int key = 0;   // key value 0
+    for(int i = 0; i < arraySize1; i++){    // copy the first number 
+        result[key++] =  array1[i];
+    }
+    for(int j = 0; j < arraySize2; j++){     // copy the secound number 
+        result[key++] = array2[j];
+
+    }
+    for (int i = 0; i < arraySize1 + arraySize2; i++){ // number1 and number 2 are added 
+    }
+    int main_array = result[arraySize1 + arraySize2];
+    float mid ;
+    if(main_array % 2 == 0){
+        mid = (result[main_array /2 -1] + result[main_array /2]) / 2.0000;
+    }else{
+        mid = (result[main_array /2] -1);
+        printf("%d",mid);
+    }
+    return mid;
+
+}
+int main(){
+    int array1[] = {1,3,5};
+    int array2[] = {2,3};
+    int arraySize1 = 3;
+    int arraySize2 = 2;
+    int call_array = array_marged_impliment(array1,arraySize1,array2,array2);
+    printf("result = %d",call_array);
+    return call_array;
 }
