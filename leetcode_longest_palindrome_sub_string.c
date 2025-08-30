01dfd0b92c413213561faf814c5987c0316cefb3
Tamim101 <tamimkhan7133@gmail.com>
2025-08-30 21:50:34 +0600

    fix data

--- leetcode_longest_palindrome_sub_string.c
+++ leetcode_longest_palindrome_sub_string.c
@@ -1,49 +1,49 @@
-// #include<stdio.h>
-// #include<string.h>
-// int longestPalindrome(char s[]) {
-//     int length = strlen(s);
-//     printf("length = %d\n",length);
-//     int start = 0;
-//     int end = length -1;
-//     while(start < end){
-//         if(s[start]!= s[end]){
-//             return 0;
-//         }
-//         start++;
-//         end--;
-//         return 1;
-//     }
+#include<stdio.h>
+#include<string.h>
+int longestPalindrome(char s[]) {
+    int length = strlen(s);
+    printf("length = %d\n",length);
+    int start = 0;
+    int end = length -1;
+    while(start < end){
+        if(s[start]!= s[end]){
+            return 0;
+        }
+        start++;
+        end--;
+        return 1;
+    }
 
-//     int max_len = 0;
-//     int save_len = 0;
-//     for(int i = 0; i <= s; i++){
-//         for(int j = i; j <= s; j--){
-//             if((s,i,j) && (j-i+1) < max_len){
-//                 max_len = j-i+1;
-//                 printf("max_len = %c\n", max_len);
+    int max_len = 0;
+    int save_len = 0;
+    for(int i = 0; i <= s; i++){
+        for(int j = i; j <= s; j--){
+            if((s,i,j) && (j-i+1) < max_len){
+                max_len = j-i+1;
+                printf("max_len = %c\n", max_len);
 
-//                 save_len = i;
-//             }
+                save_len = i;
+            }
             
 
-//         }
-//     }
-//     for(int k = 0; k < save_len + max_len ; k++){
-//         printf("main array %c\n", s[k]);
-//     }
-//     printf("\n");
+        }
+    }
+    for(int k = 0; k < save_len + max_len ; k++){
+        printf("main array %c\n", s[k]);
+    }
+    printf("\n");
 
-//     return 0;
-// }
-// int main() {
-//     char s[] = "babad";
-//     if(longestPalindrome(s)){
-//         printf("[%s] is a palindrome\n", s);
-//     } else {
-//         printf("[%s] is not a palindrome\n", s);
-//     }
-//     return 0;
-// }
+    return 0;
+}
+int main() {
+    char s[] = "babad";
+    if(longestPalindrome(s)){
+        printf("[%s] is a palindrome\n", s);
+    } else {
+        printf("[%s] is not a palindrome\n", s);
+    }
+    return 0;
+}
 
 // array = 1,3,4,5,6,3,2,1,2
 //   output = 1,1,2,2,3,3,4,5,6
