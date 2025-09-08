b440e9c8d57d94b2286e9f34ed2e03e676bb8d5a
Tamim101 <tamimkhan7133@gmail.com>
2025-09-08 14:33:25 +0600

    not done

---
+++ leetcode_ZigZag.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+char* convert(char* s, int numRows) {
+    int k ;
+    for(int i = 0;i < k;i++){
+        for(int j = j-1; j < i;j--);
+        s[j] = s[i-1]
+
+    }
+
+    
+}
+int main(){
+    char strings[20] = {"abcdefg"};
+    int numrows = 3;
+    for(char i = 0; i < numrows;i++){
+        printf("data = %s",strings[i]);
+    }
+    printf("zigzag data");
+     for(char i = 0; i < numrows;i++){
+        printf("data = %s",strings[i]);
+    }
+    return 0;
+}
