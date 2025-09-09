2e1eb46d6f5707f4a4f1bcd01c95e1d95173d64a
Tamim101 <tamimkhan7133@gmail.com>
2025-09-09 15:58:15 +0600

    fix problem

--- leetcode_ZigZag.c
+++ leetcode_ZigZag.c
@@ -1,46 +1,68 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+void init_bucket(char **bucket, int *count, int numRows,int len ){
+    for(int i = 0; i < numRows;i++){
+        bucket[i] = (char *)malloc(sizeof(char) * (len + 1));         // i create a bucket just like a matrix row
+        count[i] = 0;
+
+    }
+}
+void fill_bucket(char* s, char **bucket, int *count , int numRows){
+    int len = strlen(s);
+    int current_row = 0;
+    int ongoing_row = 1;
+    for(int i = 0; i < len; i++){
+        bucket[current_row][count[current_row]++] = s[i];  
+        if(current_row == numRows -1){           // row inishish  up ^ 
+            ongoing_row = 0;
+        }
+        else if(current_row == 0){              // row inishinsh down 
+                ongoing_row = 1;
+        }
+        current_row +=ongoing_row ? 1:-1;
+        
+    }
+}
+char *joind_bucket(char **bucket, int *count, int numRows){
+    char *result = (char*)malloc(1000);
+    result[0] = '\0';
+    for(int i = 0; i < numRows; i++){
+        bucket[i][count[i]] = '\0';  // all terminal null
+        strcat(result, bucket[i]);   // string create
+    
+    }
+    return result;
+}
 char* convert(char* s, int numRows) {
     if(numRows == 1){
-        printf("%s",&s);
-        return 0;
+        printf("%s",s);
+        return s;
     }
-    int len = strlen(s);
-    int row[numRows][len];
     char *bucket[numRows];
     int count[numRows];
-    for(int i = 0; i < numRows; i++){
-        bucket[i] = (char*)malloc((sizeof(char)*(len+1)));
-        count[i] = 0;
+    int len = strlen(s);
+    init_bucket(bucket,count,numRows,len);  //call init_bucket
+    fill_bucket(s,bucket,count,numRows);    //call fill_bucket
+     char *result = joind_bucket(bucket, count, numRows);
+
+    // Free memory for buckets
+    for(int i = 0; i < numRows; i++) {
+        free(bucket[i]);
     }
-    int current_row = 0;
-    int going_row = 1;
-    for(int i = 0; i < len;i++){
-        if(row[current_row] +i){
 
-        }else if(row[current_row -1]){
+    return result;
+    joind_bucket(bucket,count,numRows);
+    return result;
 
-        }else(row[current_row == 0]){
-
-        }
-    } 
-    current_row ++;
-    going_row --;
-    return 0;
-
-
-    
 }
-int main(){
-    char strings[] = "abcdefg";
-    int numrows = 2;
-    for(int i = 0; i < numrows;i++){
-        printf("data = %c\n",strings[i]);
-    }
-    convert(strings,numrows);
-    for(int i = 0; i < numrows;i++){
-        printf("data = %c\n",strings[i]);
-    }
+
+int main(){                               // main funshion in zigzag
+    char strings[] = "abcde";
+    int numrows = 3;
+    char *result = convert(strings,numrows);
+    printf("ZigZag data  = %s\n",result);
+    free(result);
     return 0;
 }
