26c528be4e7e915f68e6bffbb4e46f4d4f672238
Tamim101 <tamimkhan7133@gmail.com>
2025-09-11 16:34:55 +0600

    fix data

--- leetcode_reverse.c
+++ leetcode_reverse.c
@@ -1,15 +1,48 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<limits.h>
+// int reverse(int x){
+//    int sing = 1;
+//    long rev_number = 0;
+//    if(x<0){
+//      sing = -1;
+//      x = x * sing;
+//    }
+//    while(x>0){
+//       int digit = x % 10;
+//       if (rev_number > (INT_MAX - digit) / 10) {
+//             printf("0");
+//             return 0;
+//       }
+//       rev_number = rev_number * 10 + digit;
+//       x = x / 10;   
+//    }
+//    return (int)(rev_number * sing);
+
+   
+// }
+// int main(){
+//     int x = -1534236469;
+//     printf("data %d",reverse(x));
+//     return x;
+// }
 int reverse(int x){
-   int rev_number = 0;
-   while(x>0){
-      rev_number = rev_number * 10 + x % 10;
+   long rev_number = 0;
+   while(x != 0){
+      int digit = x% 10;
       x = x / 10;
+      if(rev_number > INT_MAX /10 || (rev_number == INT_MAX /10 && digit > 7)){
+         return 0;
+      }
+      if(rev_number < INT_MIN /10 || (rev_number == INT_MIN /10 && digit < -8)){
+         return 0;
+      }
+      rev_number = rev_number * 10 + digit;
    }
-   return rev_number;
+   return (int)rev_number;
 }
 int main(){
-    int x = 1320;
-    printf("data1 %d",reverse(x));
-    return x;
+    int x = 132;
+    printf("data %d",reverse(x));
+    return 0;
 }
