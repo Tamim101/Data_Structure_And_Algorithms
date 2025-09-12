9c3a9edf0654a3257b4322bca3692048a95ba264
Tamim101 <tamimkhan7133@gmail.com>
2025-09-12 18:44:26 +0600

    fix this problem

--- leetcode_climb_Stairs.c
+++ leetcode_climb_Stairs.c
@@ -1,21 +1,24 @@
 #include<stdio.h>
 int climbStairs(int n){
-    int len;
-    while(n>0 && n <= 45){
-        int total = (n + 10) / 10;
-        int lon = total+1;
-        int final = lon+1;
-        if(final== n){
-            
-            break;
-        }
-    
-    }
+   if (n <= 2){
     return n;
+   }
+   int max_value = 1;
+   int min_value = 2;
+   int total_value;
+   for(int i = 3; i <= n; i++){
+    // total = (n + 10)/ 10;
+    // equation of this problem f(n) = (n -1) = (n -2)
+    total_value = (n-1)+(n-2);
+    // max_value = min_value;
+    // min_value = total_value;
+    // printf("data = %d\n",min_value);
+   }
+   return total_value;
 }
 
 int main(){
-     int n = {54};
+     int n = {4};
      climbStairs(n);
      printf("data = %d",climbStairs(n));
      return 0;
