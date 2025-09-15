a9011a7926f161b238bbe0044e0ccf9b3898c465
Tamim101 <tamimkhan7133@gmail.com>
2025-09-15 18:54:33 +0600

    fix and added linklist

--- leetcode_climb_Stairs.c
+++ leetcode_climb_Stairs.c
@@ -35,7 +35,47 @@ int main(){
      return 0;
 }
 
+// linklist 
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct Node{
+   int data;
+   struct Node* next;
+}Node;
+Node* createNode(int data){
+   Node* newNode = (Node*)malloc(sizeof(Node));
+   if(!newNode){
+      printf("Memory allocation failed!\n");
+      exit(1);
+   }
+   newNode->data = data;
+   newNode->next = NULL;
+   return newNode;
+} 
+void print_list(Node* node){
+   while(node){
+      printf("%d->",node->data);
+      node = node->next;
+      
+   }
+   printf("null\n");
+}
 
+int main(){
+   Node* node1 = createNode(3);
+   Node* node2 = createNode(6);
+   Node* node3 = createNode(32);
+   Node* node4 = createNode(9);
 
+   node1->next = node2;
+   node2->next = node3;
+   node3->next = node4;
+
+   print_list(node1);
+   free(node1);
+   free(node2);
+   free(node3);
+   free(node4);
+}
 
 
