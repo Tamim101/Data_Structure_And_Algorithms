4f2c72d000f6b8d35e9dceb5a93503a003babb63
Tamim101 <tamimkhan7133@gmail.com>
2025-09-19 09:01:08 +0600

    update

--- doublely_linklist.c
+++ doublely_linklist.c
@@ -42,3 +42,51 @@ int main(){
     free(node3);
     return 0;
 }
+
+
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct Node{
+    int data;
+    struct Node* next;
+    struct Node* prev;
+}Node;
+int main(){
+    Node* node1 = (Node*)malloc(sizeof(Node));
+    node1->data = 3;
+
+
+    Node* node2 = (Node*)malloc(sizeof(Node));
+    node2->data = 5;
+    node2->next = NULL;
+    node2->prev = node1;
+    node1->next = node2;
+    
+    Node* node3 = (Node*)malloc(sizeof(Node));
+    node3->data = 13;
+    node3->next = NULL;
+    node3->prev = node2;
+    node2->next = node3;
+
+    Node* currentNode = node1;
+    printf("forward: ");
+    while(currentNode){
+        printf("%d ->",currentNode->data);
+        currentNode = currentNode->next;
+    }
+    printf("NULL\n");
+    currentNode = node3;
+    printf("Backward: ");
+    while(currentNode){
+        printf("%d->",currentNode->data);
+        currentNode = currentNode->prev;
+    }
+    printf("NULL\n");
+    free(node1);
+    free(node2);
+    free(node3);
+    return 0;
+}
