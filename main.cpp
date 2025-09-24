bdb0a1f379bf58e2992bc40e024cdd193ad1c6df
Tamim101 <tamimkhan7133@gmail.com>
2025-09-24 12:40:03 +0600

    done

--- main.cpp
+++ main.cpp
@@ -77,3 +77,58 @@ void setup(){
 void loop(){
 
 }
+// add queuehandle 
+
+QueueHandle_t buttonQueue;
+
+void Task_Button(void *pvParameters){
+
+  pinMode(4, INPUT_PULLUP); // button on gpio4, press = lowByte(x);
+  int msg = 1;
+  while(1){
+    if(digitalRead(4) == LOW){
+      xQueueSend(buttonQueue,&msg,portMAX_DELAY);
+      Serial.println("Button pressed and message sent to queue");
+      vTaskDelay(200 / portTICK_PERIOD_MS);
+    }
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+  }
+
+}
+
+void Task_Led(void *pvParameters){
+  pinMode(2,OUTPUT);
+  int received;
+  while(1){
+    if(xQueueReceive(buttonQueue,&received,portMAX_DELAY)== pdPASS){
+      if(received == 1){
+        digitalWrite(2,HIGH);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        digitalWrite(2, !digitalRead(2));
+        digitalWrite(2,LOW);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+      }
+    }
+  }
+}
+
+
+void Task_serial(void *pvParameters){
+  while(1){
+    Serial.println("System running");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+void setup(){
+  Serial.begin(115200);
+  buttonQueue = xQueueCreate(5,sizeof(int));
+  xTaskCreate(Task_Button,"button task",1000,NULL,1,NULL);
+  xTaskCreate(Task_Led,"led task",1000,NULL,1,NULL);
+  xTaskCreate(Task_serial,"serial task",2000,NULL,1,NULL);
+
+}
+void loop(){
+
+}
+
+
