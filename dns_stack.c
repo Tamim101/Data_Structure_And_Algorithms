909bd26ccec9af0b164316528d5f8e54d32bc859
Tamim101 <tamimkhan7133@gmail.com>
2025-10-03 19:04:57 +0600

    fix stack

--- dns_stack.c
+++ dns_stack.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 struct Node{
    char value;
    struct Node *next;
@@ -8,7 +9,7 @@ struct stack {
     int size;
 };
 struct stack *create_struck(){
-    struct stack *s = malloc(size_of(struct stack));
+    struct stack *s = malloc(sizeof(struct stack));
     s->head = NULL;
     s->size = 0;
     return s;
@@ -18,11 +19,43 @@ void push(struct stack *s, char value){
     new_node->value = value;
     new_node->next = s->head;
     s->head = new_node;
-    s->size;
+    s->size++;
 }
 char pop(struct stack *s){
-    if(s->size == 0);
-    return ' ';
+    if(s->size == 0){
+        return ' ';
+    }
+    struct Node *popped = s->head;
+    s->head = s->head->next;
+    char popped_value = popped->value;
+    free(popped);
+    s->size--;
+    return popped_value;  
 }
-struct Node *popped = 
+char peek(struct stack *s){
+    if(s->size == 0){
+        return ' ';
+    }
+    return s->head->value;
+}
+int is_empty(struct stack *s){
+    return s->size == 0;
+}  
+int stack_size(struct stack *s){
+    return s->size;
+}
+int main(){
+    struct stack *my_stack = create_struck();
+    push(my_stack,'a');
+    push(my_stack,'b');
+    push(my_stack,'c');
+
+    printf("pop: %c\n", pop(my_stack));
+    printf("peek: %c\n", peek(my_stack));
+    printf("is_empty: %d\n", is_empty(my_stack));
+    printf("size: %d\n", stack_size(my_stack));
+}
+
+
+
 
