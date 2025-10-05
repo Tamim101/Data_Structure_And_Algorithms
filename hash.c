90dadfbe1089311447d1125b18437bc399819898
Tamim101 <tamimkhan7133@gmail.com>
2025-10-05 14:10:18 +0600

    done not it

---
+++ hash.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+int hash_value(const char* value){
+    int sum = 0;
+    for(int i = 0; i < strlen(value);i++){
+        sum += (int)value[i];
+    }
+    return sum % 10;
+}
+
+int main(){
+    char value[] = "adfc";
+    printf("%s is the hash asci number: %d",value,hash_value(value));
+    return 0;
+}
+#include <stdio.h>
+#include <string.h>
+int hash_value(const char* value){
+    int sum = 0;
+    for(int i = 0; i < strlen(value);i++){
+        sum += (int)value[i];
+    }
+    return sum % 10;
+}
+
+int main(){
+    char value[] = "adfc";
+    printf("%s is the hash asci number: %d",value,hash_value(value));
+    return 0;
+}
