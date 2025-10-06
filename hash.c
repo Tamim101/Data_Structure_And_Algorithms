d6ba87a8a779b0fd54e0513b80d0ffbf10736fd5
Tamim101 <tamimkhan7133@gmail.com>
2025-10-06 12:49:56 +0600

    delete data

--- hash.c
+++ hash.c
@@ -28,3 +28,34 @@ int main(){
     printf("%s is the hash asci number: %d",value,hash_value(value));
     return 0;
 }
+
+#include <stdio.h>
+#include <string.h>
+int hash_value(const char* value){
+    int sum = 0;
+    for(int i = 0; i < strlen(value);i++){
+        sum += (int)value[i];
+    }
+    return sum % 10;
+}
+
+int main(){
+    char value[] = "adfc";
+    printf("%s is the hash asci number: %d",value,hash_value(value));
+    return 0;
+}
+#include <stdio.h>
+#include <string.h>
+int hash_value(const char* value){
+    int sum = 0;
+    for(int i = 0; i < strlen(value);i++){
+        sum += (int)value[i];
+    }
+    return sum % 10;
+}
+
+int main(){
+    char value[] = "adfc";
+    printf("%s is the hash asci number: %d",value,hash_value(value));
+    return 0;
+}
