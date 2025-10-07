4b984c9fdd91e03018cad7d661b2b34521d7fed6
Tamim101 <tamimkhan7133@gmail.com>
2025-10-07 14:00:02 +0600

    done

--- leetcode_string_to_integer.c
+++ leetcode_string_to_integer.c
@@ -1,29 +1,39 @@
-#include<stdio.h>
-#include<limits.h>
-int myAtoi(char* s){
-    if(s == NULL){
-        return ;
+#include <stdio.h>
+#include <ctype.h>   // For isspace, isdigit
+#include <limits.h>  // For INT_MIN, INT_MAX
+
+int myAtoi(char* s) {
+    if (s == NULL) {
+        return 0;
     }
-    int j = 0;
+    
+    int i = 0;
     int sign = 1;
-    while(s[i] != '\0'){
-        if(!isspace((unsigned)s[i])){
-            s[j] = s[i];
+    long result = 0; 
+    
+    // Skip leading whitespace
+    while (s[i] != '\0' && isspace((unsigned char)s[i])) {
+        i++;
+    }
+    if (s[i] == '+' || s[i] == '-') {
+        sign = (s[i] == '-') ? -1 : 1;
+        i++;
+    }
+    while (s[i] != '\0' && isdigit((unsigned char)s[i])) {
+        result = result * 10 + (s[i] - '0');
+        if (result > INT_MAX) {
+            return (sign == 1) ? INT_MAX : INT_MIN;
         }
         i++;
     }
-    s[j] = '\0';
-    if(s[i] == '+'|| s[i] == '-'){
-        sign = (s[i]=='-')? -1: 1;
-        i++;
-    }
-    while(isdigit((unsigned)s[i])){
-        int result = result * 10 + (s[i] - '0');
-        if(result > INT_MAX){
-            return (sign ==1)? INT_MAX: INT_MAX;
-        }
-        i++;
-    }
+    
     result *= sign;
+    if (result > INT_MAX) {
+        return INT_MAX;
+    }
+    if (result < INT_MIN) {
+        return INT_MIN;
+    }
+    
     return (int)result;
 }
