683284ca4c8b9f7b05d7c8310955482c70dc93ca
Tamim101 <tamimkhan7133@gmail.com>
2025-10-08 11:51:07 +0600

    hash

--- hash.c
+++ hash.c
@@ -59,3 +59,31 @@ int main(){
     printf("%s is the hash asci number: %d",value,hash_value(value));
     return 0;
 }
+#include <stdio.h>
+#include <string.h>
+
+char* myHashSet[10] = {NULL, "Jones", NULL, "Lisa", NULL, "Bob", NULL, "Siri", "Pete", NULL};
+
+int hashFunction(const char* value) {
+    int sumOfChars = 0;
+    while (*value) {
+        sumOfChars += (int)*value;
+        value++;
+    }
+    return sumOfChars % 10;
+}
+
+int contains(const char* name) {
+    int index = hashFunction(name);
+    if (myHashSet[index] != NULL && strcmp(myHashSet[index], name) == 0) {
+        return 1; // True
+    }
+    return 0; // False
+}
+
+int main() {
+    printf("'Pete' is in the Hash Set: %s\n", contains("Pete") ? "true" : "false");
+    return 0;
+}
+
+// C
